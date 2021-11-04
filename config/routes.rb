84399e09b88c539a7ac7156e06bddb6635ea1b39
MichaelSabotage <sabotageserov@gmail.com>
2021-11-04 19:11:27 +0500

    04.11changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  
-  get "/articles", to: "articles#index"
-
+  resources :articles
   # О подробностях DSL, доступного в этом файле, написано в http://rusrails.ru/rails-routing
 end
