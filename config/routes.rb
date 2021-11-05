501122bf187b7035e7818cca402128f75ea49e43
MichaelSabotage <sabotageserov@gmail.com>
2021-11-05 19:30:00 +0500

    add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # О подробностях DSL, доступного в этом файле, написано в http://rusrails.ru/rails-routing
 end
